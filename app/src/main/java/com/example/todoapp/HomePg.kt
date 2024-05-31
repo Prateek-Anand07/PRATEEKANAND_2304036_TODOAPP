2feef7817941a23d6fd352f784f289c89b5a7d11
Prateek Anand <prateek.jha2320@gmail.com>
2024-05-31 13:43:56 +0530

    adding task page added

--- app/src/main/java/com/example/todoapp/HomePg.kt
+++ app/src/main/java/com/example/todoapp/HomePg.kt
@@ -1,12 +1,16 @@
 package com.example.todoapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomePg : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,11 @@ class HomePg : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val plus = findViewById<ImageView>(R.id.plus)
+        plus.setOnClickListener {
+            val intent = Intent(this, TaskAdd::class.java)
+            startActivity(intent)
         }
     }
 }
